bc58355c79c2960adffee1763d2d8a79c309d8fe
ryanmcritchieking <ryan.mcritchie4@gmail.com>
2026-08-13 12:13:55 +1200

    buttins added use ai cos i dond to emoges

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 import customtkinter as ctk
 
 
-
+#mint ai emoges 
 buttons= [
+     "🏠  Home",
+    "🚣  Training",
+    "📝  Questionnaire",
+    "📈  Progress",
+    "🎯  Goals",
+    "👤  Profile",
+    "⚙  Settings"
 
-
-
-    
 ]
