a293e2ab777368a98cf30d31c912310885d31f2e
ryanmcritchieking <ryan.mcritchie4@gmail.com>
2026-08-13 12:20:11 +1200

    this is styling for buttions and the for buttions in buttions eans for all of them

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import customtkinter as ctk
 
-#made steeings and then go ai to fix them so they fit kniter
+#made steeings defult and then go ai to fix them so they fit kniter
 
 ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("blue")
@@ -22,3 +22,16 @@ buttons= [
     "⚙  Settings"
 
 ]
+
+
+for button in buttons:
+    nav_button = ctk.CTkButton(
+        sidebar,
+        text=button,
+        height=45,
+        corner_radius=8,
+        font=ctk.CTkFont(size=15),
+        fg_color="transparent",
+        anchor="w"
+    )
+    nav_button.pack(fill="x", padx=15, pady=4)
