c0177d3caaa7c1415371646d80e0ab51d89fcf76
ryanmcritchieking <ryan.mcritchie4@gmail.com>
2026-08-17 14:49:51 +1200

    added all planed updatd to first box and new docs

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 import customtkinter as ctk
+import sqlite3
+from datetime import datetime
 
-#made steeings defult and then go ai to fix them so they fit kniter
-
+# Dark mode for now
 ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("blue")
 
 app = ctk.CTk()
 app.title("Rowing Performance")
-app.geometry("1100x700")
-app.minsize(900, 600)
+app.geometry("1200x750")
+app.minsize(1000, 650)
+
 
 
 #mint ai emoges 
